775767fba113c9479f92052ffb281a50bf0e31a1
DESKTOP-PTD3DH4\OMEN-PC <prachya_b@hotmail.com>
2020-08-12 21:17:17 +0700

    no message

---
+++ Presentation/Nop.Web/Views/Budget/BudgetBook.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Nop.Web.Views.NewFolder
+{
+    public class BudgetBookModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
